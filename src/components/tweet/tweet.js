59abaae75b34d4c23ffa6aaa6746a335ab79950c
PieroSulca <piero.sulca2000@gmail.com>
2020-11-11 11:39:01 -0500

    add interaction in likes and retweets

--- src/components/tweet/tweet.js
+++ src/components/tweet/tweet.js
@@ -6,16 +6,65 @@ import RetweetOutline from '../../Icons/retweet.svg';
 import CommentOutline from '../../Icons/speech-bubble.svg';
 
 class Tweet extends React.Component{
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             iconRT: RetweetOutline,
             iconComment: CommentOutline,
-            iconLike: HeartOutline
+            iconLike: HeartOutline,
+            toggleRT: false,
+            toggleLike: false,
+            nRetweets: this.props.retweets,
+            nLikes: this.props.likes
         }
     }
 
+    handleClickRT = () => {
+        this.state.toggleRT ? 
+                                this.setState({
+                                    toggleRT: false,
+                                    nRetweets: this.state.nRetweets-1
+                                })
+                                :
+                                this.setState({
+                                    toggleRT: true,
+                                    nRetweets: this.state.nRetweets+1
+                                })
+    }
+
+    handleClickLike = () => {
+        this.state.toggleLike ? 
+                                this.setState({
+                                    toggleLike: false,
+                                    nLikes: this.state.nLikes-1
+                                })
+                                :
+                                this.setState({
+                                    toggleLike: true,
+                                    nLikes: this.state.nLikes+1
+                                })
+    }
+
     render(){
+
+        let _styleLike, _styleLikeIcon, _styleRT, _styleRTIcon
+        if (this.state.toggleLike){
+            _styleLike = { 
+                color: "rgb(188, 60, 106)" 
+            } 
+            _styleLikeIcon = {
+                background: 'rgb(188, 60, 106)'
+            }
+        } 
+        if (this.state.toggleRT){
+            _styleRT = { 
+                color: "rgb(24, 190, 100)" 
+            } 
+            _styleRTIcon = {
+                background: 'rgb(24, 190, 100)'
+            }
+        } 
+
         return (
             <div className="tweet-container">
                 <div className="row">
@@ -34,17 +83,17 @@ class Tweet extends React.Component{
                     </p>
                 </div>
                 <div className="reactions">
-                    <div className="row">
-                        <img className="icon-reaction comments" src={this.state.iconComment} alt="comments"></img>
+                    <div className="row comments">
+                        <img className="icon-reaction" src={this.state.iconComment} alt="comments"></img>
                         <p>{this.props.comments}</p>
                     </div>
-                    <div className="row">
-                        <img className="icon-reaction retweets" src={this.state.iconRT} alt="retweets"></img>
-                        <p>{this.props.retweets}</p>
+                    <div className="row retweets" onClick={this.handleClickRT} style={_styleRT}>
+                        <img className="icon-reaction retweets-icon" src={this.state.iconRT} alt="retweets" style={_styleRTIcon}></img>
+                        <p>{this.state.nRetweets}</p>
                     </div>
-                    <div className="row">
-                        <img className="icon-reaction likes" src={this.state.iconLike} alt="likes"></img>
-                        <p>{this.props.likes}</p>
+                    <div className="row likes" likes onClick={this.handleClickLike} style={_styleLike}>
+                        <img className="icon-reaction likes-icon" src={this.state.iconLike} alt="likes" style={_styleLikeIcon}></img>
+                        <p>{this.state.nLikes}</p>
                     </div>
                 </div>
             </div>
